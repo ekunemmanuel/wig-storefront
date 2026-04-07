8a2449424987bbada1e706c75c86e880a21ce4d3
ekunemmanuel <emmanuelapabiekun@gmail.com>
2026-04-07 10:28:13 +0100

    feat: initialize vue-router with home route and smooth scroll behavior

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,23 @@
-import { createRouter, createWebHistory } from 'vue-router'
-
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../pages/index.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: () => import('../pages/index.vue'),
+      path: "/",
+      name: "home",
+      component: Home,
     },
   ],
-})
+  scrollBehavior(to, from, savedPosition) {
+    if (to.hash) {
+      return {
+        el: to.hash,
+        behavior: "smooth",
+      };
+    }
+    return { top: 0 };
+  },
+});
 
-export default router
+export default router;
